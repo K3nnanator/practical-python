f1f5147387891a06c8fe399a07258ce6baf5d9ff
Kenny Tien <ktien@ucsc.edu>
2021-09-21 01:19:01 -0700

    test file

--- Work/sears.py
+++ Work/sears.py
@@ -5,7 +5,7 @@ num_bills = 1
 day = 1
 
 while num_bills * bill_thickness < sears_height:
-    print("day, num_bills, num_bills * bill_thickness)
+    print(day, num_bills, num_bills * bill_thickness)
     day = day + 1
     num_bills = num_bills * 2
 
